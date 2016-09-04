438d54fb50fb27c6b20e51671253ef48e6d49332
Eric Kidd <git@randomhacks.net>
2016-09-03 21:35:50 -0400

    CommandRunner: Stop abusing the borrow checker & add assert_run! macro
    Instead of doing nasty, evil things with `'a` lifetime annotations, we
    do nasty evil things with `Rc<RefCell<_>>`.  This makes our public API a
    lot easier to use, which is the important thing.
    
    We also add an `assert_run!` macro that makes it much nicer to assert
    that the expected commands were run, using a mixture of string literals,
    computed strings, and computed paths.  This also requires a bit of Rust
    magic.  But you'll like how easy it is to write and test commands.
    
    This code is worth breaking into a separate library, I think.

--- src/command_runner.rs
+++ src/command_runner.rs
@@ -1,8 +1,22 @@
 //! Utilities for running and testing shell commands.
 
-use std::ffi::OsStr;
+use std::cell::{Ref, RefCell};
+use std::ffi::{OsStr, OsString};
 use std::io;
 use std::process;
+use std::rc::Rc;
+
+/// A factory that produces objects conforming to our `Command` wrapper
+/// trait.  During tests, we'll use this to mock out the underlying system
+/// and record all commands executed.
+pub trait CommandRunner {
+    /// The type of the commands we build.  Must implement our custom
+    /// `Command` trait.
+    type Command: Command;
+
+    /// Build a new command.
+    fn build<S: AsRef<OsStr>>(&self, program: S) -> Self::Command;
+}
 
 /// A stripped down interface based on `std::process::Command`.  We use
 /// this so we can mock out shell commands during tests.
@@ -22,32 +36,13 @@ pub trait Command {
     fn status(&mut self) -> io::Result<process::ExitStatus>;
 }
 
-/// A factory that produces objects conforming to our `Command` wrapper
-/// trait.  During tests, we'll use this to mock out the underlying system
-/// and record all commands executed.
-///
-/// There's some unfortunate Rust lifetime trickiness with the `'a`
-/// parameter, which we use to indicate the idea that the `Command`
-/// returned by `build` is allowed to hold a mutable reference pointing
-/// back to the `CommandRunner`.  Usually we don't need to specify
-/// lifetimes because Rust can do all the magic in the background, but here
-/// we actually need to expose them.
-pub trait CommandRunner<'a> {
-    /// The type of the commands we build.  Must implement our custom
-    /// `Command` trait and may contain references of type `'a`.
-    type Command: Command + 'a;
-
-    /// Build a new command.
-    fn build<S: AsRef<OsStr>>(&'a mut self, program: S) -> Self::Command;
-}
-
 /// Support for running operating system commands.
 pub struct OsCommandRunner;
 
-impl<'a> CommandRunner<'a> for OsCommandRunner {
+impl CommandRunner for OsCommandRunner {
     type Command = process::Command;
 
-    fn build<S: AsRef<OsStr>>(&'a mut self, program: S) -> Self::Command {
+    fn build<S: AsRef<OsStr>>(&self, program: S) -> Self::Command {
         process::Command::new(program)
     }
 }
@@ -68,53 +63,67 @@ impl Command for process::Command {
 
 #[test]
 fn os_command_runner_runs_commands() {
-    let mut runner = OsCommandRunner;
+    let runner = OsCommandRunner;
     assert!(runner.build("true").status().unwrap().success());
     assert!(!runner.build("false").status().unwrap().success());
 }
 
 /// Support for running commands in test mode.
 pub struct TestCommandRunner {
-    cmds: Vec<Vec<String>>
+    /// The commands that have been executed.  Because we want to avoid
+    /// borrow checker hell, we use `Rc<RefCell<_>>` to implement a shared,
+    /// mutable value.
+    cmds: Rc<RefCell<Vec<Vec<OsString>>>>,
 }
 
 impl TestCommandRunner {
     /// Create a new `TestCommandRunner`.
     pub fn new() -> TestCommandRunner {
-        TestCommandRunner { cmds: vec!() }
+        TestCommandRunner { cmds: Rc::new(RefCell::new(vec!())) }
     }
 
     /// Access the list of commands run.
-    pub fn cmds(&self) -> &[Vec<String>] {
-        &self.cmds
+    pub fn cmds(&self) -> Ref<Vec<Vec<OsString>>> {
+        self.cmds.borrow()
     }
 }
 
-impl<'a> CommandRunner<'a> for TestCommandRunner {
-    type Command = TestCommand<'a>;
+impl CommandRunner for TestCommandRunner {
+    type Command = TestCommand;
 
-    fn build<S: AsRef<OsStr>>(&'a mut self, program: S) -> Self::Command {
-        self.cmds.push(vec!(program.as_ref().to_string_lossy().into_owned()));
-        TestCommand { runner: self }
+    fn build<S: AsRef<OsStr>>(&self, program: S) -> Self::Command {
+        TestCommand {
+            cmd: vec!(program.as_ref().to_owned()),
+            cmds: self.cmds.clone(),
+        }
     }
 }
 
 /// A fake command that gets logged to a `TestCommandRunner` instead of
 /// actually getting run.
-pub struct TestCommand<'a> {
-    runner: &'a mut TestCommandRunner,
+pub struct TestCommand {
+    cmd: Vec<OsString>,
+    cmds: Rc<RefCell<Vec<Vec<OsString>>>>,
 }
 
-impl<'a> Command for TestCommand<'a> {
+impl TestCommand {
+    /// Record the execution of this command.
+    fn record_execution(&self) {
+        self.cmds.borrow_mut().push(self.cmd.clone());
+    }
+}
+
+impl Command for TestCommand {
     /// Record the command arguments in our `TestCommandRunner`.
     fn arg<S: AsRef<OsStr>>(&mut self, arg: S) -> &mut Self {
-        self.runner.cmds.last_mut().unwrap()
-            .push(arg.as_ref().to_string_lossy().into_owned());
+        self.cmd.push(arg.as_ref().to_owned());
         self
     }
 
     /// Always returns success.
     fn status(&mut self) -> io::Result<process::ExitStatus> {
+        self.record_execution();
+
         // There's no portable way to build an `ExitStatus` in portable
         // Rust without actually running a command, so just choose an
         // inoffensive one with the result we want.
@@ -122,13 +131,44 @@ impl<'a> Command for TestCommand<'a> {
     }
 }
 
+/// A macro for comparing the commands that were run with the commands we
+/// hoped were run.  This is a bit trickier than you'd expect, because we
+/// need to handle two complications:
+///
+/// 1. `TestCommandRunner::cmds` returns a `Ref<_>` type, which we
+///    need to explicitly `deref()` before trying to compare, so that
+///    we get a real `&`-style reference.
+/// 2. We want to allow this macro to be passed a mix of `&'static str`
+///    and `Path` objects so that it's easier to use, and internally
+///    convert everything to an `OsString`.  So we need an internal `coerce`
+///    helper that converts from `AsRef<OsStr>` (a very general trait
+///    interface) to an actual `OsString`.
+macro_rules! assert_ran {
+    ($runner:expr, { $( [ $($arg:expr),+ ] ),* }) => {
+        use std::ops::Deref;
+        fn coerce<S: AsRef<$crate::std::ffi::OsStr>>(s: S) ->
+            $crate::std::ffi::OsString
+        {
+            s.as_ref().to_owned()
+        }
+        let expected = vec!( $( vec!( $( coerce($arg) ),+ ) ),* );
+        assert_eq!($runner.cmds().deref(), &expected);
+    }
+}
+
 #[test]
 pub fn test_command_runner_logs_commands() {
-    let mut runner = TestCommandRunner::new();
+    let runner = TestCommandRunner::new();
+
     let exit_code = runner.build("git")
         .args(&["clone", "https://github.com/torvalds/linux"])
         .status().unwrap();
     assert!(exit_code.success());
-    assert_eq!(runner.cmds(),
-               &[&["git", "clone", "https://github.com/torvalds/linux"]]);
+
+    runner.build("echo").arg("a").arg("b").status().unwrap();
+
+    assert_ran!(runner, {
+        ["git", "clone", "https://github.com/torvalds/linux"],
+        ["echo", "a", "b"]
+    });
 }
