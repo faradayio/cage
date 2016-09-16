ab6707d15a9910ddc9ed6d79226edc834f03f0b1
Eric Kidd <git@randomhacks.net>
2016-09-15 20:41:05 -0400

    Don't re-run `build.rs` unless `data/**` has changed
    This speeds up `cargo run` when working elsewhere in the tree, because
    `build.rs` doesn't need to re-run on every change anywhere in the directory.

--- build.rs
+++ build.rs
@@ -3,10 +3,16 @@
 //! source code for us to compile.
 
 extern crate includedir_codegen;
+extern crate glob;
 
 use includedir_codegen::Compression;
 
 fn main() {
+    // Don't re-run this script unless one of the inputs has changed.
+    for entry in glob::glob("data/**/*").expect("Failed to read glob pattern") {
+        println!("cargo:rerun-if-changed={}", entry.unwrap().display());
+    }
+
     // Based on example build.rs: https://github.com/tilpner/includedir
     includedir_codegen::start("DATA")
         .dir("data", Compression::None)
