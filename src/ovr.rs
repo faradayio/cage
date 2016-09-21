c4c362db4e472d208f95962fcbe02421e6806e09
Eric Kidd <git@randomhacks.net>
2016-09-21 19:45:13 -0400

    clippy: Fix markdown-related warning

--- src/ovr.rs
+++ src/ovr.rs
@@ -9,10 +9,10 @@ use project::Project;
 /// development and production.
 ///
 /// (Right now, this is deliberately a very thin wrapper around the `name`
-/// field, suitable for use as key in a BTreeMap.  If you add more fields,
-/// you'll probably need to remove `PartialEq`, `Eq`, `PartialOrd`, `Ord`
-/// from the `derive` list, and either implement them manually or redesign
-/// the code that uses overlays as hash table keys.)
+/// field, suitable for use as key in a `BTreeMap`.  If you add more
+/// fields, you'll probably need to remove `PartialEq`, `Eq`, `PartialOrd`,
+/// `Ord` from the `derive` list, and either implement them manually or
+/// redesign the code that uses overlays as hash table keys.)
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Override {
     /// The name of this environment.
