a308dfa8eb8ef92694b9db898475c6795f1d130d
Derek Kastner <dkastner@gmail.com>
2016-10-18 11:18:53 -0500

    Only allow logs command on single pods/services

--- src/cmd/logs.rs
+++ src/cmd/logs.rs
@@ -28,13 +28,24 @@ impl CommandLogs for Project {
               -> Result<()>
         where CR: CommandRunner
     {
-        let pred = |p: &Pod| p.pod_type() != PodType::Task;
-        self.compose(runner, "logs", act_on, pred, opts)
+        return match *act_on {
+            args::ActOn::All => {
+                Err("You may only specify a single service or pod".into())
+            },
+            args::ActOn::Named(ref names) => {
+                if names.len() > 1 {
+                    Err("You may only specify a single service or pod".into())
+                } else {
+                    let pred = |p: &Pod| p.pod_type() != PodType::Task;
+                    self.compose(runner, "logs", act_on, pred, opts)
+                }
+            }
+        }
     }
 }
 
 #[test]
-fn runs_docker_compose_logs_honors_enable_in_overrides() {
+fn runs_docker_compose_logs() {
     use env_logger;
     let _ = env_logger::init();
     let proj = Project::from_example("rails_hello").unwrap();
@@ -42,14 +53,12 @@ fn runs_docker_compose_logs_honors_enable_in_overrides() {
     proj.output().unwrap();
 
     let opts = args::opts::Logs::default();
-    proj.logs(&runner, &args::ActOn::All, &opts).unwrap();
+    proj.logs(
+        &runner,
+        &args::ActOn::Named(vec!("frontend".to_owned())),
+        &opts
+    ).unwrap();
     assert_ran!(runner, {
-        ["docker-compose",
-         "-p",
-         "rails_hello",
-         "-f",
-         proj.output_dir().join("pods/db.yml"),
-         "logs"],
         ["docker-compose",
          "-p",
          "rails_hello",
@@ -57,6 +66,20 @@ fn runs_docker_compose_logs_honors_enable_in_overrides() {
          proj.output_dir().join("pods/frontend.yml"),
          "logs"]
     });
+
+    proj.remove_test_output().unwrap();
+}
+
+#[test]
+fn errors_when_act_on_specifies_multiple_containers() {
+    use env_logger;
+    let _ = env_logger::init();
+    let proj = Project::from_example("rails_hello").unwrap();
+    let runner = TestCommandRunner::new();
+    proj.output().unwrap();
+
+    let opts = args::opts::Logs::default();
+    assert!(proj.logs(&runner, &args::ActOn::All, &opts).is_err());
 
     proj.remove_test_output().unwrap();
 }
